874f3c68f27f5262f4b8aef8e9592ad5b7e1ff56
xxlllq <1101464221@qq.com>
2021-07-15 17:01:24 +0800

    Word转PDF

--- PDFWater/Form1.Designer.cs
+++ PDFWater/Form1.Designer.cs
@@ -33,6 +33,8 @@
             this.result = new System.Windows.Forms.TextBox();
             this.watermarkerText = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toPDF = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // openFile
@@ -41,7 +43,7 @@
             this.openFile.Name = "openFile";
             this.openFile.Size = new System.Drawing.Size(69, 23);
             this.openFile.TabIndex = 0;
-            this.openFile.Text = "选择";
+            this.openFile.Text = "添加水印";
             this.openFile.UseVisualStyleBackColor = true;
             this.openFile.Click += new System.EventHandler(this.openFile_Click);
             // 
@@ -59,7 +61,7 @@
             this.result.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.result.Location = new System.Drawing.Point(133, 135);
+            this.result.Location = new System.Drawing.Point(133, 164);
             this.result.Multiline = true;
             this.result.Name = "result";
             this.result.Size = new System.Drawing.Size(437, 219);
@@ -84,11 +86,32 @@
             this.label1.TabIndex = 4;
             this.label1.Text = "水印文本：";
             // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(133, 135);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(437, 21);
+            this.textBox1.TabIndex = 7;
+            // 
+            // toPDF
+            // 
+            this.toPDF.Location = new System.Drawing.Point(57, 135);
+            this.toPDF.Name = "toPDF";
+            this.toPDF.Size = new System.Drawing.Size(69, 23);
+            this.toPDF.TabIndex = 6;
+            this.toPDF.Text = "转PDF";
+            this.toPDF.UseVisualStyleBackColor = true;
+            this.toPDF.Click += new System.EventHandler(this.toPDF_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.toPDF);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.watermarkerText);
             this.Controls.Add(this.result);
@@ -109,6 +132,8 @@
         private System.Windows.Forms.TextBox result;
         private System.Windows.Forms.TextBox watermarkerText;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button toPDF;
     }
 }
 
--- PDFWater/Form1.cs
+++ PDFWater/Form1.cs
@@ -41,7 +41,7 @@ namespace PDFWaterMarker
                         PdfPageBase pb = pdf.Pages.Add(); //新增一页
                         pdf.Pages.Remove(pb); //去除第一页水印
                         //创建True Type字体            
-                        PdfTrueTypeFont font = new PdfTrueTypeFont(new Font("宋体", 20f), true);
+                        PdfTrueTypeFont font = new PdfTrueTypeFont(new Font("宋体", 22f), true);
                         //水印文字            
                         string text = !string.IsNullOrEmpty(wmt) ? wmt : "仅供个人学习\n请勿用于任何商业用途";
                         //测量文字所占的位置大小，即高宽            
@@ -63,7 +63,7 @@ namespace PDFWaterMarker
                                 //将坐标系逆时针旋转45度                
                                 brush.Graphics.RotateTransform(-45);
                                 //在画刷上绘制文本                
-                                brush.Graphics.DrawString(text, font, PdfBrushes.DarkGray, 0, 0);
+                                brush.Graphics.DrawString(text, font, PdfBrushes.LightGray, 0, 0);
                                 //在PDF页面绘制跟页面一样大小的矩形，并使用定义的画刷填充                
                                 page.Canvas.DrawRectangle(brush, new RectangleF(new PointF(0, 0), page.Canvas.Size));
                             }
@@ -84,18 +84,93 @@ namespace PDFWaterMarker
         }
 
         /// <summary>
+        /// 转PDF
+        /// </summary>
+        /// <param name="path">文件夹路径</param>
+        /// <param name="wmt">水印文本</param>
+        public void ToPDF(string path,  ref string resultStr)
+        {
+            var list = new List<FileMoidel>();
+            var res = resultStr;
+
+            Director(path, list);
+            if (list != null && list.Any())
+            {
+                list.ForEach(m =>
+                {
+                    try
+                    {
+                        var filePathName = m.FullPath.Replace(path, path + "-副本");
+                        Aspose.Words.Document doc = new Aspose.Words.Document(m.FullPath);
+                        if (!string.IsNullOrEmpty(filePathName)) {
+                            filePathName = filePathName.Replace(".docx", ".pdf");
+                            filePathName = filePathName.Replace(".doc", ".pdf");
+                        }
+                        doc.Save(filePathName);
+                        res = "转换成功！";
+                    }
+                    catch (Exception ex)
+                    {
+                        res = ex.Message;
+                    }
+                });
+            }
+            resultStr = res;
+        }
+
+        private void toImage_Click(object sender, EventArgs e)
+        {
+//            var client = new RestClient("https://api.aliyundrive.com/adrive/v2/share_link/get_share_by_anonymous");
+//            client.Timeout = -1;
+//            var request = new RestRequest(Method.POST);
+//            request.AddHeader("Content-Type", "application/json");
+//            request.AddHeader("Cookie", "acw_tc=2f6a1f9c16263269778963337e47548f9a053d4d6e3c4fb6b155654f17df7c");
+//            var body = @"{
+//" + "\n" +
+//            @"    ""share_id"":""moB5Z35cViv""
+//" + "\n" +
+//            @"}";
+//            request.AddParameter("application/json", body, ParameterType.RequestBody);
+//            IRestResponse response = client.Execute(request);
+//            Console.WriteLine(response.Content);
+            //Apply();
+        }
+        public void Apply() {
+            var inputFile = @"C:\Users\xxx\source\repos\xxlllq\system_architect\真题(截至2020年)【推荐】\历年真题及解析合并版本（截至2020年）\2009年\2009年系统架构师考试科目三：论文真题.pdf";
+            //创建PdfDocument对象            
+            PdfDocument doc = new PdfDocument();
+            //加载现有PDF文档
+            doc.LoadFromFile(inputFile);
+            //PdfPageBase pb = doc.Pages.Add(); //新增一页
+            //doc.Pages.Remove(pb); //去除第一页水印
+            for (int i = 0; i < doc.Pages.Count; i++)
+            {
+                Image img = doc.SaveAsImage(i);
+                string fn1 = String.Format("{0}.png", i + 1);
+                var outputFile = inputFile.Substring(0, inputFile.LastIndexOf(".pdf"));
+                if (Directory.Exists(outputFile) == false)
+                {
+                    Directory.CreateDirectory(outputFile);
+                }
+                string of = Path.Combine(outputFile+"\\", fn1);
+                img.Save(of, System.Drawing.Imaging.ImageFormat.Png);
+            }
+            var sd = 1;
+        }
+
+        /// <summary>
         /// 递归获取文件
         /// </summary>
         /// <param name="dir"></param>
         /// <param name="list"></param>
-        public void Director(string dir, List<FileMoidel> list)
+        public void Director(string dir, List<FileMoidel> list,bool chooseDoc = false)
         {
             DirectoryInfo d = new DirectoryInfo(dir);
             FileInfo[] files = d.GetFiles();//文件
             DirectoryInfo[] directs = d.GetDirectories();//文件夹
             foreach (FileInfo f in files)
             {
-                if (f?.Extension == ".pdf")
+                if (f?.Extension == ".pdf" || (chooseDoc = true && (f?.Extension == ".docx" || f?.Extension == ".doc")))
                 {
                     list.Add(new FileMoidel { FullPath = f.FullName, FolderName = f.Directory.Name, FileName = f.Name });//添加文件名到列表中  
                 }
@@ -129,6 +204,39 @@ namespace PDFWaterMarker
                     if (!string.IsNullOrEmpty(path))
                     {
                         ApplyWatermark(path, wmt,ref resultStr);
+                    }
+                    return resultStr;
+                };
+                wait.BeginInvoke(new AsyncCallback(res =>
+                {
+                    string rst = wait.EndInvoke(res);
+                    this.Invoke((Action)(() => result.Text = rst));
+                }), null);
+
+            }
+        }
+
+        /// <summary>
+        /// 转PDF
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void toPDF_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var path = dialog.SelectedPath;
+                textBox1.Text = path;
+                result.Text = "开始转换…";
+
+                Func<string> wait = () =>
+                {
+                    var resultStr = string.Empty;
+                    if (!string.IsNullOrEmpty(path))
+                    {
+                        ToPDF(path, ref resultStr);
                     }
                     return resultStr;
                 };
